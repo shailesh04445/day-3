0277574ba1f4e6854bda218495694671a2afcb9c
shailesh04445 <100185744+shailesh04445@users.noreply.github.com>
2022-02-27 13:02:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let xyz = null;
+let abc = "";
+if (xyz == abc) 
+{
+    console.log("true");
+}
+else 
+{console.log("false")};
+let xyz=null;
+let abc="";
+if (xyz==abc)
+{
+    console.log("true");
+}
+else
+{console.log("false")};
+
+let num=520;
+if (num %2===0){
+    console.log("even");
+}
+else {console.log(odd)}
+;
+
+let x=10;
+x=x+1;
+x+=1;
+x++;
+x-= 1;
+console.log(x);
